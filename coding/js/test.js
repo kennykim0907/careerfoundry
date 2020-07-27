9dcd6a1a722f225caf28bd13466a8bd452c7a44f
kennykim0907 <55411757+kennykim0907@users.noreply.github.com>
2020-07-27 16:03:55 -0400

    2.2 Update

--- coding/js/test.js
+++ coding/js/test.js
@@ -1,21 +1,21 @@
 /* TASK 1*/
-// function getValue() {
-//   var y = document.getElementById("boo");
-//   var x = document.getElementById("foo").value;
-//   if (x == "") {
-//     window.alert((y.innerHTML = "Please try again!"));
-//     // window.alert('Enter your name');
-//     return false;
-//   } else if(x === "Bobby"){
-//     window.alert((y.innerHTML = "Bobby is not allowed to use it as your name"));
-//   } else if (x.length > 20) {
-//     window.alert((y.innerHTML = "Your name should be less than 20 characters"));
-//     return false;
-//   } else {
-//     y.innerHTML = x + ` ` + `is a good name!`;
-//     return true;
-//   }
-// }
+function getValue() {
+  var y = document.getElementById("boo");
+  var x = document.getElementById("foo").value;
+  if (x == "") {
+    window.alert((y.innerHTML = "Please try again!"));
+    // window.alert('Enter your name');
+    return false;
+  } else if(x === "Bobby"){
+    window.alert((y.innerHTML = "Bobby is not allowed to use it as your name"));
+  } else if (x.length > 20) {
+    window.alert((y.innerHTML = "Your name should be less than 20 characters"));
+    return false;
+  } else {
+    y.innerHTML = x + ` ` + `is a good name!`;
+    return true;
+  }
+}
 
 
 /* TASK 2*/
@@ -29,21 +29,16 @@
 //     }
 // }
 
-// const button = document.getElementById("start");
-
-// button.onclick = function startButtonClicked() {
-
-// };
 
 
 /* TASK 3*/
-var a = 100;
-var sum = 0;
-const button = document.getElementById('start');
+// var a = 100;
+// var sum = 0;
+// const button = document.getElementById('start');
 
-button.onclick = function startButtnClicked(){
-    for(a = 100; a <= 200; a++){
-        sum = sum+a;
-        document.write(sum + ' ');
-    }
-}
+// button.onclick = function startButtnClicked(){
+//     for(a = 100; a <= 200; a++){
+//         sum = sum+a;
+//         document.write(sum + ' ');
+//     }
+// }
