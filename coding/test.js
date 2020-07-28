e39fa9383cc93a2b3ce57f98741c7f0c6c1fca51
kennykim0907 <55411757+kennykim0907@users.noreply.github.com>
2020-07-28 04:04:59 -0400

    Update

--- coding/test.js
+++ coding/test.js
@@ -1,11 +1,44 @@
+/* TASK 1*/
 function getValue() {
   var y = document.getElementById("boo");
   var x = document.getElementById("foo").value;
   if (x == "") {
-    y.innerHTML = "Please enter a name";
+    window.alert((y.innerHTML = "Please try again!"));
+    // window.alert('Enter your name');
+    return false;
+  } else if(x === "Bobby"){
+    window.alert((y.innerHTML = "Bobby is not allowed to use it as your name"));
+  } else if (x.length > 20) {
+    window.alert((y.innerHTML = "Your name should be less than 20 characters"));
     return false;
   } else {
-    y.innerHTML = `That is a good name!` + ` ` + x;
+    y.innerHTML = x + ` ` + `is a good name!`;
     return true;
   }
 }
+
+
+/* TASK 2*/
+// const button = document.getElementById("start");
+
+// button.onclick = function checkNumber(){
+//     for(let i = 0; i <= 99; i++){
+//         if((i == 0) || (i > 90) || (i >= 10 && i <= 20) || (i >= 42 && i <= 43) || (i >= 60 && i <= 80)){
+//             document.write(i + ', ');
+//         }
+//     }
+// }
+
+
+
+/* TASK 3*/
+// var a = 100;
+// var sum = 0;
+// const button = document.getElementById('start');
+
+// button.onclick = function startButtnClicked(){
+//     for(a = 100; a <= 200; a++){
+//         sum = sum+a;
+//         document.write(sum + ' ');
+//     }
+// }
