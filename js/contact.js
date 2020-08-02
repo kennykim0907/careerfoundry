a917268fbcb91616cd1fa7f682191e76b9c174c8
kennykim0907 <55411757+kennykim0907@users.noreply.github.com>
2020-08-02 16:25:04 -0400

    update

--- js/contact.js
+++ js/contact.js
@@ -4,3 +4,50 @@ const menu = document.querySelector(".navbar_menu");
 toggleBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+/* TASK 1*/
+function getValue() {
+  var y = document.getElementById("boo");
+  var x = document.getElementById("foo_1").value;
+  if (x == "") {
+    window.alert((y.innerHTML = "Please try again!"));
+    // window.alert('Enter your name');
+    return false;
+  } else if (x.length > 20) {
+    window.alert((y.innerHTML = "Your name should be less than 20 characters"));
+    return false;
+  } else {
+    window.alert((y.innerHTML = "I will get back to you as soon as I can!"));
+    return false;
+  }
+
+  function getValue() {
+    var y = document.getElementById("boo");
+    var x = document.getElementById("foo_2").value;
+    if (x == "") {
+      window.alert((y.innerHTML = "Please try again!"));
+      // window.alert('Enter your name');
+      return false;
+    } else if (x.length > 20) {
+      window.alert((y.innerHTML = "Your name should be less than 20 characters"));
+      return false;
+    } else {
+      window.alert((y.innerHTML = "I will get back to you as soon as I can!"));
+      return false;
+    }
+
+    function getValue() {
+      var y = document.getElementById("boo");
+      var x = document.getElementById("foo_3").value;
+      if (x == "") {
+        window.alert((y.innerHTML = "Please try again!"));
+        // window.alert('Enter your name');
+        return false;
+      } else if (x.length > 20) {
+        window.alert((y.innerHTML = "Your name should be less than 20 characters"));
+        return false;
+      } else {
+        window.alert((y.innerHTML = "I will get back to you as soon as I can!"));
+        return false;
+      }
+}
